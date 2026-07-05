cdbb7647f09e6d9864b7e669dd3768946e039495
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-07-05 13:41:21 -0500

    fixed fill and clip rule naming

--- packages/website/src/components/EqualIcon.tsx
+++ packages/website/src/components/EqualIcon.tsx
@@ -11,8 +11,8 @@ export function EqualIcon(props: ComponentProps<"svg">) {
       {...props}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M1.5 5C1.22386 5 1 5.22386 1 5.5C1 5.77614 1.22386 6 1.5 6H13.5C13.7761 6 14 5.77614 14 5.5C14 5.22386 13.7761 5 13.5 5H1.5ZM1 9.5C1 9.22386 1.22386 9 1.5 9H13.5C13.7761 9 14 9.22386 14 9.5C14 9.77614 13.7761 10 13.5 10H1.5C1.22386 10 1 9.77614 1 9.5Z"
         fill="currentColor"
       />
